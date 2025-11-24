b2e29b61950d164848e7a74f9e9054cf7bf7770e
nicesoft-labs <me@sbelikov.ru>
2025-11-25 01:26:38 +0300

    Update Footer.tsx

--- webapp/src/components/Footer.tsx
+++ webapp/src/components/Footer.tsx
@@ -7,6 +7,7 @@ export function Footer() {
       <div class="fw-semibold">ООО "НАЙС СОФТ ГРУП"</div>
       <div>
         Работает на <a href="https://niceos.ru" target="_blank" rel="noreferrer">НАЙС.ОС</a>
+        Используйте наши другие продукты: <a href="https://yandex.cloud/ru/marketplace?publishers=f2ed90kua4t10varbjh9&tab=software" target="_blank" rel="noreferrer">на Яндекс.Cloud</a>
       </div>
       <div>NiceSoft.Eve © {new Date().getFullYear()}</div>
     </footer>
